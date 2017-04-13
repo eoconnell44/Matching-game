70cc2cea900c5021a4480a7bef6b7f8fd97c0499
Ed O'Connell <eoconnell44@gmail.com>
2017-04-13 15:36:49 -0400

    added clock timer

--- js/card.js
+++ js/card.js
@@ -4,55 +4,80 @@ let cardsArray = [];
 //Leave empty array to push selected cards into
 let cardsInPlay = [];
 let cards = ['vv', 'vv',3,3,5,5,7,7];
+let createCard;
+// let board = document.querySelectorAll('.cards');
+let board;
+let card;
+let isMatch = document.querySelectorAll('div .active');
+
 //Add a difficulty setting to the game
 // let difficulty = {
 // 		easy: cards(8),
 // 		medium: cards(12),
 // 		hard: cards(20)
 // };
-
-//For later use of removing event listener
-let board = document.querySelectorAll('.cards');
-
 function startGame() {
 //Limit only 1 board created at a time
-
 	let btn = document.querySelector('button');
 		btn.addEventListener('click', createBoard);
-		if(createBoard > 1) {
-			btn.removeEventListener('click', createBoard);
-			// cell.removeEventListener('click', playCell);
-		}
 
 }
+/*// parg is an object reference to a <p> element
+
+if (parg.hasChildNodes()) {
+  // So, first we check if the object is not empty, if the object has child nodes
+  var children = parg.childNodes;
+
+  for (var i = 0; i < children.length; i++) {
+    // do something with each child as children[i]
+    // NOTE: List is live, Adding or removing children will change the list
+  }
+}*/
 
 function createBoard() {
-	for(var i = 0; i < cards.length; i++){
+	for(let i = 0; i < cards.length; i++){
 		let createCard = document.createElement('div');
 			createCard.setAttribute('class', 'cards');
-			createCard.setAttribute('data-id', i);
+			createCard.setAttribute('id', i);
 			createCard.addEventListener('click', flipCard);
 		document.querySelector('.gameBoard').appendChild(createCard);
 	}
-};
+	countDown();
+removeEL();
 
+};
+//For later use of removing event listener
 //Getting a TypeError: board.removeEventListener is not a function...Need to do some research into this, not sure why..
 function removeEL(){
-	board.removeEventListener('click', flipCard);
+	// board.removeEventListener('click', flipCard);
+// 		if(createBoard.hasChildNodes()) {
+// 	let children = createBoard.childNodes;
+
+// 	for(var i = 0; i< children.length; i++){
+// 		children[i].removeEventListener('click', createBoard);
+// 	}
+// }
 		
 }
 
-function flipCard() {
+function flipCard(e) {
+	board = document.querySelectorAll('.cards');	
 	//Need to grab the cards specific id
-	let card = this.getAttribute('data-id');
+	// let card = this.getAttribute('data-id');
+	let card = e.target;
+	// console.log(firstCard);
+	let cardIdtag = e.target.getAttribute('id');
+	console.log(card);
 	// this.innerHTML = '<img src="assets/vvaughn.jpg">';
 	//Changing BGC without toggleClass....possibly do an add/remove w/ multy lines of code
-	this.style.backgroundColor = 'gray';
-	cardsInPlay.push(cards[card]);
-	// cardsInPlay.push(this);
-	console.log(card);
+	// this.style.backgroundColor = 'gray';
+	this.className = 'active';
+	cardsInPlay.push(cards[cardIdtag]);
+
 	console.log(cardsInPlay);
-	console.log(cardsInPlay[0]);
+
+	// firstCard = this.cardsInPlay;
+	// console.log(firstCard);
 
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
@@ -62,15 +87,29 @@ function flipCard() {
 	return			
 }
 
-function checkForMatch() {
+function checkForMatch() {	
 //For testing, matches are 1-2, 3-4, 5-6, 7-8
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		console.log('match!');
-		let match = (cardsInPlay[0], cardsInPlay[1]);
-		console.log(match);
+		for(let i =0; i <cardsInPlay.length; i++){
+			let changeClass = document.querySelectorAll('.active');
+					changeClass.setAttribute('class', 'isMatch');
+					console.log(changeClass)
+			console.log(cardsInPlay.length);
+			// cardsInPlay.setAttribute('class','.isMatch');
+		}
+
+		// isMatch.style.backgroundColor = "red";
+			// isMatch.className = 'isMatch';
+			cardsInPlay = [];
+		// console.log(isMatch);
+		// console.log(cardsInPlay);
+		// cardUno = this.cardsInPlay;
+		// 	console.log(cardUno);
+		// this.cardsInPlay.style.backgroundColor = 'black';
+		// cardsInPlay[1].style.backgroundColor = 'black';
 	} else {
-		cardsInPlay.style.backgroundColor = 'black';
-		resetGame();
+		cardsInPlay = [];
 	}
 	
 }
@@ -78,14 +117,27 @@ function checkForMatch() {
 function resetGame() {
 	//clears cards showing
 	cardsInPlay = [];
+	//Removes all pieces on game board
+	location.reload();
 
 }
+let startTime = 60;
+let timeCounter = document.querySelector('.timer');
+//Display initial number 
+timeCounter.innerHTML = startTime;
+
+function countDown() {
+	//Needed to have ability to clear later on
+	let time = setInterval(function(){
+timeCounter.innerHTML = startTime;
+		startTime -= 1;
+		if(startTime < 0){
+			clearInterval(time);
+		}
+		}, 1000);
+	}
 
 function difficulty() {
-
-}
-
-function setTimer() {
 
 }
 
--- js/main.js
+++ js/main.js
@@ -7,49 +7,85 @@ let cardArray = document.querySelectorAll('div');
 			console.log(cardArray[i])
 		}
 	})
+let cards = ['vv', 'vv',3,3,5,5,7,7];
+let cardsInPlay = [];
+let isMatch = document.querySelectorAll('div .active');
+let currentCard = document.querySelector('.active');
+// let btn = document.querySelector('button');
 
-let button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-  startGame();
-});
+// button.addEventListener('click', () => {
+//   startGame();
+// });
 	
 class startGame {
-	constructor(cardArray) {
-		this.cardArray = [];
-		this.flipCard(cardArray);
-		this.selectedCards = [];
-		console.log(startGame);
+		constructor() {
+		this.cardArray = null;
+		// this.createBoard();
+		this.cardsInPlay= null;
+		this.selectedCards = null;
+		console.log('startGame');
+		// this.checkForMatch = null;
+		// this.checkforWin = null;
+		this.btn = document.querySelector('button').addEventListener('click', this.createBoard)
+			// this.createBoard();
 
 	}
+		createBoard() {
+			console.log('board created');
+				for(let i = 0; i < cards.length; i++){
+		let createCard = document.createElement('div');
+			createCard.setAttribute('class', 'cards');
+			createCard.setAttribute('id', i);
+			
+		document.querySelector('.gameBoard').appendChild(createCard);
+		createCard.addEventListener('click', this.flipCard);
+			console.log(createCard);
+			console.log(cards);
+
+	}
+		}
+		checkForMatch() {
+			if(this.cardsInPlay[0] === this.cardsInPlay[1]) {
+			console.log('match!');
+		}
+
 	//Insert all card randomization here
 	// createBoard() {
 
-	// }
+	}
  flipCard(card){
- 	this.selectedCards.push(card);
-	this.card.setAttribute('class', 'active');
-	console.log(card);
-	console.log(selectedCards);
+ 	let cardIdtag = this.getAttribute('id');
+ 	this.className = 'active';
+ 	cardsInPlay.push(cards[cardIdtag]);
+	console.log(this.card);
+
+		if(cardsInPlay.length === 2) {
+		this.checkForMatch();
+		// removeEL();
+		// debugger;
+	} 
+	return
 
 }
 
-	checkForMatch() {
-		// for(var i = 0; i < selectedCards.length; i++) {
-			if(selectedCards[0] === selectedCards[1]) {
-				console.log('Cards match!');
-		} else {
-				console.log('Not a match!');
-		}
-	}
+	// checkForMatch() {
+
+
+	// }
 
 	checkforWin() {
+		console.log('you win!');
 
 	}
 
 }
-let newG = new startGame();
-Card();
+
+// checkForMatch();
+// checkforWin();
+
+window.onload = function() {
+  game = new startGame();
+};
 // class Card {
 
 // }
