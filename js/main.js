01aed37850dd413fd86e5ca0a71744d583b531c6
Ed O'Connell <eoconnell44@gmail.com>
2017-06-08 11:03:26 -0400

    code cleanup, yellow button

--- js/main.js
+++ js/main.js
@@ -18,82 +18,58 @@ let currentCard = document.querySelector('.active');
 // });
 	
 class startGame {
-		constructor() {
-		this.cardArray = null;
+	constructor() {
+	this.cardArray = null;
+	// this.createBoard();
+	this.cardsInPlay= null;
+	this.selectedCards = null;
+	console.log('startGame');
+	// this.checkForMatch = null;
+	// this.checkforWin = null;
+	this.btn = document.querySelector('button').addEventListener('click', this.createBoard)
 		// this.createBoard();
-		this.cardsInPlay= null;
-		this.selectedCards = null;
-		console.log('startGame');
-		// this.checkForMatch = null;
-		// this.checkforWin = null;
-		this.btn = document.querySelector('button').addEventListener('click', this.createBoard)
-			// this.createBoard();
 
 	}
-		createBoard() {
-			console.log('board created');
-				for(let i = 0; i < cards.length; i++){
+	createBoard() {
+		console.log('board created');
+		for(let i = 0; i < cards.length; i++){
 		let createCard = document.createElement('div');
-			createCard.setAttribute('class', 'cards');
-			createCard.setAttribute('id', i);
-			
+		createCard.setAttribute('class', 'cards');
+		createCard.setAttribute('id', i);
 		document.querySelector('.gameBoard').appendChild(createCard);
 		createCard.addEventListener('click', this.flipCard);
-			console.log(createCard);
-			console.log(cards);
-
-	}
+		console.log(createCard);
+		console.log(cards);
 		}
-		checkForMatch() {
-			if(this.cardsInPlay[0] === this.cardsInPlay[1]) {
-			console.log('match!');
-		}
-
-	//Insert all card randomization here
-	// createBoard() {
-
 	}
+
+	checkForMatch() {
+		if(this.cardsInPlay[0] === this.cardsInPlay[1]) {
+		console.log('match!');
+		}
+	}
+
  flipCard(card){
  	let cardIdtag = this.getAttribute('id');
  	this.className = 'active';
  	cardsInPlay.push(cards[cardIdtag]);
 	console.log(this.card);
-
 		if(cardsInPlay.length === 2) {
 		this.checkForMatch();
 		// removeEL();
 		// debugger;
 	} 
 	return
-
 }
-
-	// checkForMatch() {
-
-
-	// }
-
 	checkforWin() {
 		console.log('you win!');
-
 	}
-
 }
-
 // checkForMatch();
 // checkforWin();
-
 window.onload = function() {
   game = new startGame();
 };
-// class Card {
 
-// }
 
-// document.querySelector('button').addEventListener('click', startGame)
-// cardArray.addEventListener('click', )
 
-// 	let boxArray = document.querySelectorAll('td');
-// Array.from(boxArray).forEach(function(el) {
-// 	for(var i =0; i <boxArray.length; i++) {
-// 	let topRow = boxArray[i];
